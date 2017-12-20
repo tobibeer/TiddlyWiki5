b018738b64bd5cead2776874136f98d18849e569
Tobias Beer <beertobias@gmail.com>
2017-12-20 22:23:28 +0100

    tentative proposal for restructured tw.com

---
+++ editions/buildall.ps1
@@ -0,0 +1,21 @@
+cd tw5.tw5
+tiddlywiki --output "../../" --build index
+cd..
+cd tw5.docs
+tiddlywiki --output "../../" --build index
+cd..
+cd tw5.dev
+tiddlywiki --output "../../" --build index
+cd..
+cd tw5.tips
+tiddlywiki --output "../../" --build index
+cd..
+cd tw5.node
+tiddlywiki --output "../../" --build index
+cd..
+cd tw5.engage
+tiddlywiki --output "../../" --build index
+cd..
+cd tw5.plugins
+tiddlywiki --output "../../" --build index
+cd..
